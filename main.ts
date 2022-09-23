2ff5ef64556434e3e0e37cda3c905ec49777f63a
brunoho88 <114266743+brunoho88@users.noreply.github.com>
2022-09-24 07:28:47 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let degree = 0
+let strip: neopixel.Strip = null
+let range: neopixel.Strip = null
+input.onButtonPressed(Button.A, function () {
+    degree = input.compassHeading()
+    if (degree < 45) {
+        basic.showString("N")
+        basic.showArrow(ArrowNames.North)
+    } else if (degree < 135) {
+        basic.showString("E")
+        basic.showArrow(ArrowNames.East)
+    } else if (degree < 225) {
+        basic.showString("S")
+        basic.showArrow(ArrowNames.South)
+    } else {
+        basic.showString("W")
+        basic.showArrow(ArrowNames.West)
+    }
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) > 0) {
+        strip = neopixel.create(DigitalPin.P1, 7, NeoPixelMode.RGB)
+        range = strip.range(0, 7)
+        range.showRainbow(1, 360)
+    } else {
+        strip = neopixel.create(DigitalPin.P1, 7, NeoPixelMode.RGB)
+        range = strip.range(0, 7)
+        range.clear()
+        range.show()
+    }
+    pins.analogWritePin(AnalogPin.P12, pins.analogReadPin(AnalogPin.P2))
+})
